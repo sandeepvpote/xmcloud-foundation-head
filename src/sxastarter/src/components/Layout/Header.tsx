e26699dd9b6fa9f2c3cff23234452075cfc0dafa
Pranavi Pradhan <pranavi.pradhan@assurex.co>
2024-04-24 19:05:50 +0530

    use effect removed

--- src/sxastarter/src/components/Layout/Header.tsx
+++ src/sxastarter/src/components/Layout/Header.tsx
@@ -3,7 +3,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/no-unescaped-entities */
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Field,
   ImageField,
