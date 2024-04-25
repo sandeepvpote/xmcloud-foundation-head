5c1401113a8746c8ec46f18656183d6b6fcefd1b
Sandeep Pote <techy.sandeeppote@gmail.com>
2024-04-25 11:03:11 +0100

    FE fix

--- src/sxastarter/src/components/Common/header/HeaderStripe.tsx
+++ src/sxastarter/src/components/Common/header/HeaderStripe.tsx
@@ -1,6 +1,3 @@
-import { useRouter } from 'next/router';
-
-import ALink from '../custom-link';
 /*
 import CartMenu from '~/components/common/partials/cart-menu';
 import MainMenu from '~/components/common/partials/main-menu';
@@ -9,7 +6,7 @@ import LoginModal from '~/components/features/modals/login-modal';
 
 import { headerTransprentList } from '~/utils/data/menu';*/
 
-export default function HeaderStripe(props) {
+export default function HeaderStripe() {
   /*const router = useRouter();
 
   const showMobileMenu = () => {
@@ -26,64 +23,14 @@ export default function HeaderStripe(props) {
     <div className="header-top header header-transparent p-relative bg-dark">
       <div className="container-fluid">
         <div className="header-left">
-          <div className="social-links inline-links d-lg-show">
-            <ALink
-              href="#"
-              className="social-link social-twitter fab fa-twitter"
-              title="twitter"
-            ></ALink>
-            <ALink
-              href="#"
-              className="social-link social-linkedin fab fa-linkedin-in"
-              title="linkedin"
-            ></ALink>
-            <ALink
-              href="#"
-              className="social-link social-facebook fab fa-facebook-f"
-              title="facebook"
-            ></ALink>
-            <ALink
-              href="#"
-              className="social-link social-pinterest fab fa-pinterest-p"
-              title="pinterest"
-            ></ALink>
-          </div>
+          <div className="social-links inline-links d-lg-show"></div>
           <p className="welcome-msg ls-normal">Welcome to store message or remove it!</p>
         </div>
 
         <div className="header-right">
-          <ALink className="call d-lg-show" href="#">
-            <span>Call us: </span>123-456-7890
-          </ALink>
-          <div className="dropdown ml-4 d-lg-show">
-            <ALink href="#">USD</ALink>
-            <ul className="dropdown-box">
-              <li>
-                <ALink href="#">USD</ALink>
-              </li>
-              <li>
-                <ALink href="#">EUR</ALink>
-              </li>
-            </ul>
-          </div>
+          <div className="dropdown ml-4 d-lg-show"></div>
           <span className="divider ml-3 d-lg-show"></span>
-          <div className="dropdown language-dropdown d-lg-show">
-            <ALink href="#">ENG</ALink>
-            <ul className="dropdown-box">
-              <li>
-                <ALink href="#">ENG</ALink>
-              </li>
-              <li>
-                <ALink href="#">FRH</ALink>
-              </li>
-            </ul>
-          </div>
-          <ALink
-            href="/pages/contact-us"
-            className="btn btn-primary btn-sm contact font-weight-normal mr-0 ml-4"
-          >
-            Contact us
-          </ALink>
+          <div className="dropdown language-dropdown d-lg-show"></div>
         </div>
       </div>
     </div>
